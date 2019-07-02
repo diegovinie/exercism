242bf3ba14dd5cbdd24fe92bb7924c75f59eea0f
Diego Viniegra <diegovillalobos@catedra.edu.co>
2019-07-02 16:51:11 -0500

    2 rna-transcription correction

--- rna-transcription/rna_transcription.exs
+++ rna-transcription/rna_transcription.exs
@@ -7,18 +7,15 @@ defmodule RNATranscription do
   iex> RNATranscription.to_rna('ACTG')
   'UGAC'
   """
-  defp dna_rna_eq, do: %{
-    "A" => "U",
-    "C" => "G",
-    "T" => "A",
-    "G" => "C"
-  }
-
   @spec to_rna([char]) :: [char]
-  def to_rna(dna), do: dna
-    |> List.to_string
-    |> String.codepoints
-    |> Enum.map(fn code -> dna_rna_eq[code] end)
-    |> List.to_charlist
+  def to_rna(dna) do
+    dna_rna_eq = %{
+      ?A => ?U,
+      ?C => ?G,
+      ?T => ?A,
+      ?G => ?C
+    }
 
+    Enum.map dna, fn code -> dna_rna_eq[code] end
+  end
 end

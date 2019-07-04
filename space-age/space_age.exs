fa3dcae95fe9c39289456fed5479d5058925c3f7
Diego Viniegra <diegovillalobos@catedra.edu.co>
2019-07-04 15:23:56 -0500

    space_age: atoms shorcut in map

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -10,14 +10,14 @@ defmodule SpaceAge do
           | :neptune
 
   @rel_earth %{
-    :mercury  => 0.2408467,
-    :venus    => 0.61519726,
-    :earth    => 1,
-    :mars     => 1.8808158,
-    :jupiter  => 11.862615,
-    :saturn   => 29.447498,
-    :uranus   => 84.016846,
-    :neptune  => 164.79132
+    mercury:  0.2408467,
+    venus:    0.61519726,
+    earth:    1,
+    mars:     1.8808158,
+    jupiter:  11.862615,
+    saturn:   29.447498,
+    uranus:   84.016846,
+    neptune:  164.79132
   }
 
   @doc """

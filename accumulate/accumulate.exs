0e93d1411065f3bc04a396d6a15fe253aa2742d6
Diego Viniegra <diegovillalobos@catedra.edu.co>
2019-07-08 10:08:09 -0500

    accumulate: 1st submit

---
+++ accumulate/accumulate.exs
@@ -0,0 +1,25 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate(list, fun) do
+    map(list, fun)
+  end
+
+  defp map([], _fun), do: []
+  defp map([head | tail], fun), do: [fun.(head) | map(tail, fun)]
+end

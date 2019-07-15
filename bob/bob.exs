4fd443112e92f4b8c28faa0d8c2bafa61d9c66ed
Diego Viniegra <diego.viniegra@gmail.com>
2019-07-15 07:17:57 -0500

    bob final

--- bob/bob.exs
+++ bob/bob.exs
@@ -2,20 +2,20 @@ defmodule Bob do
 
   def hey(input) do
     cond do
-      is_question(input) and is_yelling(input) ->
+      question?(input) and yelling?(input) ->
         "Calm down, I know what I'm doing!"
-      is_question(input)  -> "Sure."
-      is_yelling(input)   -> "Whoa, chill out!"
+      question?(input)    -> "Sure."
+      yelling?(input)     -> "Whoa, chill out!"
       say_nothing?(input) -> "Fine. Be that way!"
       true                -> "Whatever."
     end
   end
 
-  defp is_question(text), do: String.ends_with? text, "?"
+  defp question?(text), do: String.ends_with? text, "?"
 
-  defp is_yelling(text), do: letters?(text) and text === String.upcase(text)
+  defp yelling?(text), do: letters?(text) and text == String.upcase(text)
 
-  defp say_nothing?(text), do: "" === String.trim text
+  defp say_nothing?(text), do: "" == String.trim text
 
-  defp letters?(text), do: !(String.upcase(text) === String.downcase(text))
+  defp letters?(text), do: !(String.upcase(text) == String.downcase(text))
 end

cd208c5f85bbe7339f8239e3d55585d24ba952e3
Diego Viniegra <diego.viniegra@gmail.com>
2019-08-06 12:57:04 -0500

    bank-account 1st submission

--- bank-account/lib/bank_account.ex
+++ bank-account/lib/bank_account.ex
@@ -13,6 +13,33 @@ defmodule BankAccount do
   """
   @spec open_bank() :: account
   def open_bank() do
+    {:ok, pid} = Task.start_link(fn -> account(0, true) end)
+    pid
+  end
+
+  def account(amount, is_open) when is_open == true do
+    receive do
+      {caller, :get} ->
+        send caller, {self(), {:ok, amount}}
+        account(amount, is_open)
+      {caller, {:put, value}} ->
+        send caller, {self(), :ok}
+        account(amount + value, is_open)
+      {caller, :stop} ->
+        send caller, {self(), :ok}
+        account(amount, false)
+    end
+  end
+
+  def account(amount, is_open) do
+    receive do
+      {caller, :start} ->
+        send caller, {self(), :ok}
+        account(amount, true)
+      {caller, _} ->
+        send caller, {self(), {:error, :account_closed}}
+        account(amount, is_open)
+    end
   end
 
   @doc """
@@ -20,6 +47,11 @@ defmodule BankAccount do
   """
   @spec close_bank(account) :: none
   def close_bank(account) do
+    send account, { self(), :stop}
+    receive do
+      {^account, :ok} -> :ok
+      {^account, other} -> other
+    end
   end
 
   @doc """
@@ -27,6 +59,11 @@ defmodule BankAccount do
   """
   @spec balance(account) :: integer
   def balance(account) do
+    send account, {self(), :get}
+    receive do
+      {^account, {:ok, balance}} -> balance
+      {^account, other}          -> other
+    end
   end
 
   @doc """
@@ -34,5 +71,10 @@ defmodule BankAccount do
   """
   @spec update(account, integer) :: any
   def update(account, amount) do
+    send account, {self(), {:put, amount}}
+    receive do
+      {^account, :ok}    -> :ok
+      {^account, other}  -> other
+    end
   end
 end

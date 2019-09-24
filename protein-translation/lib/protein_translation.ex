39345544c9f6d271a1e32567db0d1f5d90162580
Diego Viniegra <diego.viniegra@gmail.com>
2019-09-24 15:38:35 -0500

    protein-translation: 1st submission

--- protein-translation/lib/protein_translation.ex
+++ protein-translation/lib/protein_translation.ex
@@ -1,9 +1,38 @@
 defmodule ProteinTranslation do
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
   @doc """
-  Given an RNA string, return a list of proteins specified by codons, in order.
+  Given an "RNA" string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    chain = rna
+      |> to_charlist
+      |> Enum.chunk_every(3)
+      |> Enum.map(&(&1 |> to_string |> of_codon))
+      |> Enum.take_while(&check_stop/1)
+
+    case Enum.any?(chain, &check_rna_error/1) do
+      true  -> {:error, "invalid RNA"}
+      false -> {:ok, Enum.map(chain, fn {_, protein} -> protein end)}
+    end
   end
 
   @doc """
@@ -29,5 +58,15 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    case @codons[codon] do
+      nil     -> {:error, "invalid codon"}
+      protein -> {:ok, protein}
+    end
   end
+
+  @spec check_stop({any, String.t()}) :: boolean
+  defp check_stop({_, status}), do: status != "STOP"
+
+  @spec check_rna_error({:atom, any}) :: boolean
+  defp check_rna_error({status, _}), do: status == :error
 end
